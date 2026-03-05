659803de2cae7511ed1fd796c10e98b4b175fac3
Shamsiddin <xamroqulovsh7@gmail.com>
2026-03-05 16:18:33 +0500

    Bugs fixed

--- src/start.js
+++ src/start.js
@@ -1,15 +1,18 @@
 import { texts } from "./constant/index.js";
 import { keyboards } from "./keyboard/index.js";
+import { config } from "dotenv";
+config();
 
 export const startHandler = async (ctx) => {
-  // const chatId = ctx.message.chat.id;
+  const chatId = ctx.message.chat.id;
   const type = ctx.message.chat.type;
 
-  // if(chatId !== process.env.ADMIN_ID) return ctx.reply(texts.admin.uz.not_admin)
-
   if (type !== "private") return;
+
+  if (chatId !== Number(process.env.DEV_ID) && chatId !== Number(process.env.ADMIN_ID)) return ctx.reply(texts.admin.uz.not_admin);
+
   ctx.reply(texts.admin.uz.welcome, {
     reply_markup: keyboards.admin.uz.sendPriceKeyboard(),
-    parse_mode: "Markdown"
+    parse_mode: "Markdown",
   });
 };
